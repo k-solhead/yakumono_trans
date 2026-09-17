9a8ab0f538a8a7060cde89fce6718c8d5e842271
k-solhead <hyodo.ka@gmail.com>
2026-09-17 15:18:25 +0900

    Fix whitespace handling in text stripping

--- contents/en.py
+++ contents/en.py
@@ -406,7 +406,7 @@ else:
                     if _elem.tag == qn('w:t') and _elem.text:
                         _prev_t = _elem
                     elif _elem.tag == qn('w:br') and _prev_t is not None:
-                        stripped = _prev_t.text.rstrip(' 	')
+                        stripped = _prev_t.text.rstrip(' \t')
                         if stripped != _prev_t.text:
                             _prev_t.text = stripped
 
